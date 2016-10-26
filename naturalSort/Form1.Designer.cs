c904ac6104627e1bd149f612114423df1f4fc15a
Anton Kokhanchik <kokhanchik.tversu@gmail.com>
2016-10-26 11:56:07 +0300

    bla

--- naturalSort/Form1.Designer.cs
+++ naturalSort/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace naturalSort
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Требуется переменная конструктора.
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openFileButton.Location = new System.Drawing.Point(83, 52);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(181, 89);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Выбрать файл";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // sortButton
+            // 
+            this.sortButton.Enabled = false;
+            this.sortButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sortButton.Location = new System.Drawing.Point(83, 174);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(181, 88);
+            this.sortButton.TabIndex = 1;
+            this.sortButton.Text = "Сортировать";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(350, 324);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "mainForm";
+            this.Text = "Естественная сортировка";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button sortButton;
     }
 }
 
--- naturalSort/Form1.cs
+++ naturalSort/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,39 @@ using System.Windows.Forms;
 
 namespace naturalSort
 {
-    public partial class Form1 : Form
+    public partial class mainForm : Form
     {
-        public Form1()
+        FileStream file;
+        public mainForm()
         {
             InitializeComponent();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                file = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.ReadWrite);
+                sortButton.Enabled = true;
+            }
+
+
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            runSort();
+        }
+
+        void runSort()
+        {
+            FileStream fileA = new FileStream("fileA", FileMode.CreateNew, FileAccess.ReadWrite);
+            FileStream fileB = new FileStream("fileB", FileMode.CreateNew, FileAccess.ReadWrite);
+
+            while(true)
+            {
+
+            }
         }
     }
 }
