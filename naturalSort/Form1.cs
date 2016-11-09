53c87c25f044f7c4f35780d34a0b2fb1f68ace73
Anton Kokhanchik <kokhanchik.tversu@gmail.com>
2016-11-09 12:55:00 +0400

    moving next

--- naturalSort/Form1.cs
+++ naturalSort/Form1.cs
@@ -40,10 +40,52 @@ namespace naturalSort
             FileStream fileA = new FileStream("fileA", FileMode.CreateNew, FileAccess.ReadWrite);
             FileStream fileB = new FileStream("fileB", FileMode.CreateNew, FileAccess.ReadWrite);
 
-            while(true)
-            {
+            StreamReader f = new StreamReader(file);
+            StreamWriter a = new StreamWriter(fileA);
+            StreamWriter b = new StreamWriter(fileB);
 
+            string prev = "";
+			bool isA = true;
+
+            while(!f.EndOfStream)
+            {
+                char[] buffer = new char[1000];
+                f.ReadBlock(buffer, 0, 1000);
+
+                string[] tmpStringsArray = buffer.ToString().Split(' ');
+
+                if (buffer[0] != ' ')
+                    tmpStringsArray[0] = prev + tmpStringsArray[0];
+
+				prev = tmpStringsArray.Last();
+
+				int tmp= Int32.Parse(tmpStringsArray[0]);
+				a.Write(tmpStringsArray[0] + " ");
+
+				for (int i = 1; i < tmpStringsArray.Length; i++)
+				{
+					if (Int32.Parse(tmpStringsArray[i]) < tmp)
+						if (isA)
+							isA = false;
+						else
+							isA = true;
+
+					if (isA)
+						a.Write(tmpStringsArray[i].ToString() + " ");
+					else
+						b.Write(tmpStringsArray[i].ToString() + " ");
+					tmp = Int32.Parse(tmpStringsArray[i]);
+				}
             }
+
+			StreamWriter f0 = new StreamWriter(file);
+			StreamReader a0 = new StreamReader(fileA);
+			StreamReader b0 = new StreamReader(fileB);
+
+			while(!a0.EndOfStream || !b0.EndOfStream)
+			{
+				
+			}
         }
     }
 }
