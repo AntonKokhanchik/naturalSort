1c38b1d88ffc13ba3276a3060e81e248f1a07785
Kokhanchik Anton <kokhanchik.tversu@gmail.com>
2016-11-27 16:07:06 +0300

    elder versions returned (to look at)

--- naturalSort/Form1.cs
+++ naturalSort/Form1.cs
@@ -68,14 +68,42 @@ namespace naturalSort
 			}
 		}
 
+		//private bool isSorted()
+		//{
+		//	using (StreamReader f = new StreamReader(openFileDialog1.FileName))
+		//	{
+		//		string prev = "";
+
+		//		char[] buffer = new char[1000];
+
+		//		while (!f.EndOfStream)
+		//		{
+		//			f.ReadBlock(buffer, 0, 1000);
+
+		//			if (buffer[0] != ' ')
+		//				buffer = (prev + buffer).ToCharArray();
+		//			else
+		//				if (prev != "")
+		//					buffer = (prev + " " + buffer).ToCharArray();
 
 
+		//			string[] tmpStringsArray = buffer.ToString().Split(' ');
+
+		//			if (buffer.Last() == ' ')
+		//				prev = "";
+		//			else
+		//				prev = tmpStringsArray.Last();
 
 
-
-
-
-
+		//			for (int i = 0; i < tmpStringsArray.Length - 1; i++)//внимание
+		//			{
+		//				if (Int32.Parse(tmpStringsArray[i]) > Int32.Parse(tmpStringsArray[i + 1]))
+		//					return false;
+		//			}
+		//		}
+		//		return true;
+		//	}
+		//}
 
 		private string readWord(StreamReader stream)
 		{
@@ -131,6 +159,163 @@ namespace naturalSort
 			}
 		}
 
+		//private void distribute()
+		//{
+		//	using (StreamReader f = new StreamReader(openFileDialog1.FileName))
+		//	using (StreamWriter a = new StreamWriter("FileA", false))
+		//	using (StreamWriter b = new StreamWriter("FileB", false))
+		//	{
+		//		bool isA = true; //определяет направление записи
+
+		//		while (!f.EndOfStream)
+		//		{
+		//			char[] buffer = new char[100];
+		//			f.ReadBlock(buffer, 0, 100);
+
+		//			// разобъём на подстроки по пробелам
+		//			string[] tmpStringsArray = String.Concat(buffer).Split(' ', '\0', '\n', '\r');
+		//			tmpStringsArray = tmpStringsArray.Where(str => str != "").ToArray();
+
+		//			// записываем первый символ, чтобы получить с чем сравнивать - tmp
+		//			int tmp = Int32.Parse(tmpStringsArray[0]);
+		//			a.Write(tmpStringsArray[0] + " ");
+
+		//			for (int i = 1; i < tmpStringsArray.Length - 1; i++)
+		//			{
+		//				// перенаправляем запись, если следующий элемент меньше предыдущего
+		//				if (Int32.Parse(tmpStringsArray[i]) < tmp)
+		//					if (isA)
+		//						isA = false;
+		//					else
+		//						isA = true;
+
+		//				if (isA)
+		//					a.Write(tmpStringsArray[i] + " ");
+		//				else
+		//					b.Write(tmpStringsArray[i] + " ");
+
+		//				tmp = Int32.Parse(tmpStringsArray[i]);
+		//			}
+
+
+		//			if(Int32.Parse(tmpStringsArray.Last()) < tmp)
+		//				if (isA)
+		//					isA = false;
+		//				else
+		//					isA = true;
+
+		//			string last;
+
+		//			if (buffer.Last() == ' ')
+		//				last = tmpStringsArray.Last() +  " ";
+		//			else
+		//				last = tmpStringsArray.Last();
+
+		//			if (isA)
+		//				a.Write(last);
+		//			else
+		//				b.Write(last);
+		//		}
+		//	}
+		//}
+
+		//private void assamblyng()
+		//{
+		//	using (StreamWriter f0 = new StreamWriter(openFileDialog1.FileName))
+		//	using (StreamReader a0 = new StreamReader("fileA"))
+		//	using (StreamReader b0 = new StreamReader("fileB"))
+		//	{
+		//		string prevA = "";
+		//		string prevB = "";
+
+
+		//		while (!a0.EndOfStream || !b0.EndOfStream)
+		//		{
+		//			char[] bufferA = new char[1000];
+		//			char[] bufferB = new char[1000];
+		//			a0.ReadBlock(bufferA, 0, 1000);
+		//			b0.ReadBlock(bufferB, 0, 1000);
+
+		//			string[] tmpStringsArrayA = bufferA.ToString().Split(' ');
+		//			string[] tmpStringsArrayB = bufferB.ToString().Split(' ');
+
+		//			if (bufferA[0] != ' ')
+		//				tmpStringsArrayA[0] = prevA + tmpStringsArrayA[0];
+		//			if (bufferB[0] != ' ')
+		//				tmpStringsArrayB[0] = prevB + tmpStringsArrayB[0];
+
+		//			if (bufferA.Last() == ' ')
+		//				prevA = "";
+		//			else
+		//				prevA = tmpStringsArrayA.Last();
+		//			if (bufferB.Last() == ' ')
+		//				prevB = "";
+		//			else
+		//				prevB = tmpStringsArrayB.Last();
+
+		//			int i = 0, j = 0;
+		//			while (i < tmpStringsArrayA.Length - 1 || j < tmpStringsArrayB.Length - 1)
+		//			{
+		//				if (Int32.Parse(tmpStringsArrayA[i]) < Int32.Parse(tmpStringsArrayB[j]))
+		//				{
+		//					f0.Write(tmpStringsArrayA[i] + " ");
+		//					i++;
+		//				}
+		//				else
+		//				{
+		//					f0.Write(tmpStringsArrayB[j] + " ");
+		//					j++;
+		//				}
+		//			}
+		//		}
+
+		//		if (a0.EndOfStream)
+		//		{
+		//			char[] bufferB = new char[1000];
+		//			b0.ReadBlock(bufferB, 0, 1000);
+		//			string[] tmpStringsArrayB = bufferB.ToString().Split(' ');
+
+		//			if (bufferB[0] != ' ')
+		//				tmpStringsArrayB[0] = prevB + tmpStringsArrayB[0];
+
+		//			if (bufferB.Last() == ' ')
+		//				prevB = "";
+		//			else
+		//				prevB = tmpStringsArrayB.Last();
+
+		//			int j = 0;
+
+		//			while (j < tmpStringsArrayB.Length - 1)
+		//			{
+		//				f0.Write(tmpStringsArrayB[j] + " ");
+		//				j++;
+		//			}
+		//		}
+		//		else
+		//		{
+		//			char[] bufferA = new char[1000];
+		//			b0.ReadBlock(bufferA, 0, 1000);
+		//			string[] tmpStringsArrayA = bufferA.ToString().Split(' ');
+
+		//			if (bufferA[0] != ' ')
+		//				tmpStringsArrayA[0] = prevA + tmpStringsArrayA[0];
+
+		//			if (bufferA.Last() == ' ')
+		//				prevA = "";
+		//			else
+		//				prevA = tmpStringsArrayA.Last();
+
+		//			int j = 0;
+
+		//			while (j < tmpStringsArrayA.Length - 1)
+		//			{
+		//				f0.Write(tmpStringsArrayA[j] + " ");
+		//				j++;
+		//			}
+		//			f0.Flush();
+		//		}
+		//	}
+		//}
 		private void assemble()
 		{
 			using (StreamWriter f = new StreamWriter(openFileDialog1.FileName, false))
